9465bf50c3efbfdd4d63ecd7fd89a37c90627418
Jordan <jordan.beary@outlook.com>
2017-12-06 23:51:49 -0800

    git push origin master

--- data-raw/mod_filepath.R
+++ data-raw/mod_filepath.R
@@ -1,3 +1,3 @@
 setwd("C:\\Users\\J-Blazer\\Documents\\glpkAPI.DEA\\data-raw")
-mod_filepath <- file("C:\\Users\\J-Blazer\\Documents\\glpkAPI.DEA\\mod.files\\glpkAPIDEA.mod")
-devtools::use_data(mod_filepath)
+testmod_filepath <- file("C:\\Users\\J-Blazer\\Documents\\glpkAPI.DEA\\mod.files\\glpkAPIDEA.mod")
+devtools::use_data(testmod_filepath)
