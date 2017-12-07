423069f334080e107d64c3cc09713f15c2d7ccad
Jordan <jordan.beary@outlook.com>
2017-12-07 09:50:06 -0800

    git push origin master

--- data-raw/airline_datapath.R
+++
@@ -1,3 +0,0 @@
-setwd("C:\\Users\\J-Blazer\\Documents\\glpkAPI.DEA\\data-raw")
-testairline_datapath <- file("C:\\Users\\J-Blazer\\Documents\\glpkAPI.DEA\\dat.files\\airline.dat.dat")
-devtools::use_data(testairline_datapath)
--- data-raw/mod_filepath.R
+++
@@ -1,3 +0,0 @@
-setwd("C:\\Users\\J-Blazer\\Documents\\glpkAPI.DEA\\data-raw")
-testmod_filepath <- file("C:\\Users\\J-Blazer\\Documents\\glpkAPI.DEA\\mod.files\\glpkAPIDEA.mod")
-devtools::use_data(testmod_filepath)
